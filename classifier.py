aa6c2474d99a8855c811f87acf4d6571f5773ac9
Daniel Gelderblom <d.gelderblom1908@gmail.com>
2025-06-09 22:41:26 +0200

    Dev: hyperparameters and evaluation

--- classifier.py
+++ classifier.py
@@ -1,21 +1,56 @@
 import pickle
-import numpy as np
-import pandas as pd
-import random
 from imblearn.under_sampling import RandomUnderSampler
+from imblearn.over_sampling import RandomOverSampler
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn import preprocessing
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV, cross_val_score, train_test_split, StratifiedKFold
 from sklearn.pipeline import make_pipeline
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_report, confusion_matrix, balanced_accuracy_score, accuracy_score
+from sklearn.metrics import ConfusionMatrixDisplay, classification_report, confusion_matrix
 import matplotlib.pyplot as plt
+import time
+
+
+RANDOM_STATE = 42
+
+
+def train_pipeline(model):
+    """Input: a scikit-learn machine learning class.
+    Returns a pipeline with a scaler and the class."""
+    return make_pipeline(preprocessing.StandardScaler(), model)
+
 
 def train(model, X, y):
-    pipe = make_pipeline(preprocessing.StandardScaler(), model)
+    """Inputs: a scikit-learn machine learning class, data and labels.
+    Returns a classifier fitted to data and labels."""
+    pipe = train_pipeline(model)
     pipe.fit(X, y)
     return pipe
+
+
+def evaluate(algorithm_name, grids, X_test, y_test):
+    """Input: the algorithm name, a dictionary of GridSearchCV objects, test data and labels
+    Prints the classification report, the best hyperparameters.
+    Saves a confusion matrix for the best model"""
+    for dataset in grids:
+        grid = grids[dataset]
+        y_pred = grid.best_estimator_.predict(X_test)
+        print(f"report for {algorithm_name} with training set {dataset}:")
+        print(classification_report(y_test, y_pred, digits=3))
+        print("Best parameters:", grids[dataset].best_params_)
+        print()
+
+    labels = ["CDU/CSU", "SPD", "GRÜNE", "LINKE", "AfD", "FDP"]
+    cm = confusion_matrix(y_test, y_pred, labels=labels, normalize='true')
+    disp_cm = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=labels)
+
+    disp_cm.plot(cmap='Blues')
+    plt.title(f"Confusion Matrix for {algorithm_name}")
+    plt.savefig(f"confusion_matrix_{algorithm_name}.png")
+    plt.close()
+
+
 
 
 with open('feature_matrices.pkl', 'rb') as infile:
@@ -23,80 +58,123 @@ with open('feature_matrices.pkl', 'rb') as infile:
 
 # remove very small groups (further explanation in written report)
 parties_to_remove = ["BSW", "Die Linke", "Fraktionslos"]
-filtered_feature_df = feature_df[~feature_df["Party"].isin(parties_to_remove)]
-X = filtered_feature_df.drop(columns=["Party"])
-y = filtered_feature_df["Party"]
+filtered_feature_df = feature_df[~feature_df["party"].isin(parties_to_remove)]
+X = filtered_feature_df.drop(columns=["party"])
+y = filtered_feature_df["party"]
 
 
-# baseline: 0.26412491377688596
-X_train, X_devtest, y_train, y_devtest = train_test_split(X, y, test_size=0.3)
-X_dev, X_test, y_dev, y_test = train_test_split(X_devtest, y_devtest, test_size=0.33)
+# Perform train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=RANDOM_STATE)
 
 
+# Investigate the features
+
+# Cross-validation setup
+cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=RANDOM_STATE)
+
+# Set the class_weight parameter in order to not favor the majority class
+full_model = train_pipeline(LogisticRegression(class_weight='balanced'))
+full_score = cross_val_score(full_model, X, y, cv=cv, scoring='f1_macro').mean()
+print(f"macro F1-score with all features: {full_score}")
+print()
+
+# check for every feature if the classifier is better without it
+for col in X.columns:
+    X_mod = X.drop(columns=[col])
+    model_mod = train_pipeline(LogisticRegression(class_weight='balanced'))
+    score_mod = cross_val_score(model_mod, X_mod, y, cv=cv, scoring='f1_macro').mean()
+    print(f"macro F1-score without feature {col}: {score_mod}")
+    if score_mod > full_score:
+        print(f"Better without feature {col}: {score_mod:.4f} > {full_score:.4f}")
 
 
-# pipe_logrec = train(LogisticRegression(), X_train, y_train)
-# all_features_score = pipe_logrec.score(X_test, y_test)
-# n_features = len(X_train[0])
-# for feature in range(n_features):
-#     X_train_modified = [X_train[sample][0:feature]+X_train[sample][feature+1:n_features] for sample in X_train]
-#     X_dev_modified = [X_dev[sample][0:feature]+X_dev[sample][feature+1:n_features] for sample in X_dev]
-#     pipe_logrec_modified = train(LogisticRegression(), X_train_modified, y_train)
-#     modified_features_score = pipe_logrec.score(X_dev, y_dev)
-#     if modified_features_score > all_features_score:
-#         print(f"The score is better without feature {feature}.")
-
-
-# building a balanced training set
-
-rus = RandomUnderSampler(random_state=42)
-
+# Build balanced training sets
 # Apply undersampling
-X_train_balanced, y_train_balanced = rus.fit_resample(X_train, y_train)
+rus = RandomUnderSampler(random_state=RANDOM_STATE)
+X_train_undersampled, y_train_undersampled = rus.fit_resample(X_train, y_train)
 
-# Combine features and labels into one balanced DataFrame
-#balanced_feature_matrices = pd.concat([X_balanced_df, y_balanced_df], axis=1)
-
-# Display class distribution
-print(y_train_balanced.value_counts())
-print(y_train.value_counts())
+# Apply oversampling
+ros = RandomOverSampler(random_state=RANDOM_STATE)
+X_train_oversampled, y_train_oversampled = ros.fit_resample(X_train, y_train)
 
 
+train_datasets = {"unbalanced": [X_train, y_train],
+                  "undersampled": [X_train_undersampled, y_train_undersampled],
+                  "oversampled": [X_train_oversampled, y_train_oversampled]}
+
+
+# Perform tuning of hyperparameters with a grid search
+now = time.time()
+# Logistic Regression
+
+param_grid_lr = {
+    'C': [0.01, 0.1, 1, 10],
+    'penalty': ['l1', 'l2'],
+    'class_weight': ['balanced', None],
+    'solver': ['liblinear']
+}
+
+grids_lr = {}
+for dataset in train_datasets.keys():
+    grids_lr[dataset] = GridSearchCV(
+        # without setting the maximum iterations, there is not convergence
+        LogisticRegression(max_iter=1000, random_state=42),
+        param_grid_lr,
+        scoring='f1_macro',
+        cv=5
+    )
+
+    grids_lr[dataset].fit(train_datasets[dataset][0], train_datasets[dataset][1])
+    print(time.time() - now)
+
+
+# Support Vector Classifier
+
+param_grid_svc = {
+    'C': [0.1, 1, 10],
+    'kernel': ['linear', 'rbf'],
+    'gamma': ['scale', 'auto'],
+    'class_weight': ['balanced', None]
+}
+
+grids_svc = {}
+for dataset in train_datasets.keys():
+    grids_svc[dataset] = GridSearchCV(
+        SVC(random_state=42),
+        param_grid_svc,
+        scoring='f1_macro',
+        cv=5
+    )
+
+    grids_svc[dataset].fit(train_datasets[dataset][0], train_datasets[dataset][1])
+    print(time.time() - now)
 
 
 
+# Random Forest Classifier
 
-pipe_logrec = train(LogisticRegression(), X_train, y_train)
-y_pred = pipe_logrec.predict(X_dev)
-print(confusion_matrix(y_dev, y_pred, labels=["CDU/CSU","SPD","BÜNDNIS 90/DIE GRÜNEN","DIE LINKE","AfD","FDP"]))
-print(f"Accuracy for Logistic regression (unbalanced dataset): {accuracy_score(y_dev, y_pred)}")
-print(f"Balanced accuracy for Logistic regression (unbalanced dataset): {balanced_accuracy_score(y_dev, y_pred)}")
+param_grid_rfc = {
+    'n_estimators': [100, 300],
+    'max_depth': [10],
+    'max_features': ['sqrt', 'log2'],
+    'min_samples_leaf': [2, 4],
+    'class_weight': [None, 'balanced']
+}
 
+grids_rfc = {}
+for dataset in train_datasets.keys():
+    grids_rfc[dataset] = GridSearchCV(
+        RandomForestClassifier(random_state=42),
+        param_grid_rfc,
+        scoring='f1_macro',
+        cv=5
+    )
 
-pipe_logrec_balanced = train(LogisticRegression(), X_train_balanced, y_train_balanced)
-y_pred_balanced = pipe_logrec_balanced.predict(X_dev)
-print(confusion_matrix(y_dev, y_pred_balanced, labels=["CDU/CSU","SPD","BÜNDNIS 90/DIE GRÜNEN","DIE LINKE","AfD","FDP"]))
-print(f"Accuracy for Logistic regression (balanced dataset): {accuracy_score(y_dev, y_pred_balanced)}")
-print(f"Balanced accuracy for Logistic regression (balanced dataset): {balanced_accuracy_score(y_dev, y_pred_balanced)}")
-
-
-pipe_svm = train(SVC(), X_train, y_train)
-y_pred_svm = pipe_svm.predict(X_dev)
-print(f"Accuracy for SVM (unbalanced dataset): {accuracy_score(y_dev, y_pred_svm)}")
-print(f"Balanced accuracy for SVM (unbalanced dataset): {balanced_accuracy_score(y_dev, y_pred_svm)}")
-
-
-pipe_svm_balanced = train(SVC(), X_train_balanced, y_train_balanced)
-y_pred_svm_balanced = pipe_svm_balanced.predict(X_dev)
-print(f"Accuracy for SVM (balanced dataset): {accuracy_score(y_dev, y_pred_svm_balanced)}")
-print(f"Balanced accuracy for SVM (balanced dataset): {balanced_accuracy_score(y_dev, y_pred_svm_balanced)}")
+    grids_rfc[dataset].fit(train_datasets[dataset][0], train_datasets[dataset][1])
+    print(time.time() - now)
 
 
-
-pipe_rfc = train(RandomForestClassifier(), X_train, y_train)
-print(pipe_rfc.score(X_test, y_test))
-
-#balanced score?
-
-
-
+# Evaluate all three algorithms
+evaluate("logistic_regression", grids_lr, X_test, y_test)
+evaluate("svc", grids_svc, X_test, y_test)
+evaluate("random_forest", grids_rfc, X_test, y_test)
